588bb4d3c9d6acfbc65663b44484124b3b70f229
Narendra Kamath G <narendrakamathg@gmail.com>
2020-04-17 20:53:47 +0530

    Updates

---
+++ precache-manifest.bd80a6897c40df8e12d4ed46aa97a35c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ba1e91674a4d02c27494d2b4354377b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ed415186db472f6f1c2d",
+    "url": "/monsters-rolodex/static/css/main.1e534220.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed415186db472f6f1c2d",
+    "url": "/monsters-rolodex/static/js/main.90bebc5e.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
